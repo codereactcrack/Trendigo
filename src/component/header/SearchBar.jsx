5e25df69b129e3c8e79c6cc18acef73889fbc107
CodeReactCrack <codereactcrack@gmail.com>
2024-06-26 00:12:07 +0530

    Search BAR -UI CSS FILE

--- src/component/header/SearchBar.jsx
+++ src/component/header/SearchBar.jsx
@@ -4,40 +4,43 @@ import useFetchCollection from '../../hooks/useFetchCollection';
 import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
 import { useNavigate } from 'react-router-dom';
 import SearchContext from '../../context/Search/SearchContext';
+import './css/SearchBar.css';
 
 const SearchBar = () => {
-    const {input,setInput} = useContext(SearchContext);
+    const {input, setInput} = useContext(SearchContext);
     const productList = useFetchCollection('product-list');
     let filterList;
-    if(input){
-      filterList = productList.filter(data => (data.name).toLowerCase().includes(input.toLowerCase()));
+    if (input) {
+        filterList = productList.filter(data => (data.name).toLowerCase().includes(input.toLowerCase()));
     }
-
     const navigate = useNavigate();
-  return (
-    <div>
-        <SearchIcon className="header__search-icon" />
-        <input type="text" placeholder="Search Item.." className="header__search-input"  
-        onChange={(e)=>setInput(e.target.value) }/>
 
-        <div className='search-list'>
-          {input && filterList.map((data)=>{
-            return (
-              <div>
-                <div onClick={()=>navigate(`shop/${data.id}`)}>
-                  <SearchIcon />
-                  {data.name} 
+    return (
+        <div className="header__search-bar">
+            <SearchIcon className="header__search-icon" />
+            <input
+                type="text"
+                placeholder="Search Item.."
+                className="header__search-input"
+                onChange={(e) => setInput(e.target.value)}
+            />
+            {input && (
+                <div className="search-list">
+                    {filterList.map((data) => (
+                        <div className="search-list-item" key={data.id}>
+                            <div className="search-list-item-content" onClick={() => navigate(`shop/${data.id}`)}>
+                                <SearchIcon />
+                                {data.name}
+                            </div>
+                            <div className="search-list-item-close" onClick={() => setInput('')}>
+                                <CloseRoundedIcon />
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                <div onClick={()=>setInput()}>
-                  <CloseRoundedIcon/>
-                </div>
-              </div>
-            )
-          })}
+            )}
         </div>
+    );
+};
 
-    </div>
-  )
-}
-
-export default SearchBar
+export default SearchBar;
