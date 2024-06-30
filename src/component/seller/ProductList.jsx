a1c1ce83b1be57f4efb726e728ab2375f1ccca6a
CodeReactCrack <codereactcrack@gmail.com>
2024-06-30 08:17:42 +0530

    Product List Form error code and add to database and CSS

--- src/component/seller/ProductList.jsx
+++ src/component/seller/ProductList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useForm, useFieldArray } from 'react-hook-form';
 import './css/ProductList.css';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../services/firebase';
 
 const ProductList = () => {
   const { register, handleSubmit, control, formState: { errors } } = useForm();
@@ -12,8 +14,29 @@ const ProductList = () => {
     control,
     name: 'specifications',
   });
+  const productRef = collection(db, 'product-list');
 
-  const onSubmit = (data) => console.log(data);
+  async function onSubmit(data) {
+    const finalPrice = data.price - (data.price * (data.discount / 100));
+
+    // Map images and specifications to the desired structure
+    const images = data.images.map(image => image.url);
+    const specifications = data.specifications.map(spec => spec.spec);
+
+    await addDoc(productRef, {
+      brand: data["product-brand"],
+      category: data["product-category"],
+      description: data["product-description"],
+      discount: data.discount || 0,
+      price: data.price,
+      finalPrice,
+      images,
+      name: data["product-name"],
+      specification: specifications,
+      stock: data.stock,
+      featured: data.featured || false,  // Default to false if not specified
+    });
+  }
 
   return (
     <div className="product-list-container">
@@ -22,33 +45,34 @@ const ProductList = () => {
         <label htmlFor="product-name">Name of Product</label>
         <input {...register("product-name", { required: "Product name is required", maxLength: { value: 99, message: "Maximum length is 99 characters" } })} name="product-name" className="input-field" />
         {errors["product-name"] && <p className="error-message">{errors["product-name"].message}</p>}
-        
+
         <label htmlFor="product-brand">Brand Name</label>
         <input {...register("product-brand", { required: "Brand name is required", maxLength: { value: 99, message: "Maximum length is 99 characters" } })} name="product-brand" className="input-field" />
         {errors["product-brand"] && <p className="error-message">{errors["product-brand"].message}</p>}
-        
+
         <label htmlFor="product-description">Product Description</label>
         <textarea {...register("product-description", { required: "Product description is required" })} name="product-description" className="input-field" />
         {errors["product-description"] && <p className="error-message">{errors["product-description"].message}</p>}
-        
+
         <label htmlFor="product-category">Category</label>
         <select {...register("product-category", { required: "Category is required" })} className="input-field">
-          <option value="female">Female</option>
-          <option value="male">Male</option>
+          <option value="women">women</option>
+          <option value="men">men</option>
           <option value="furniture">Furniture</option>
           <option value="beauty">Beauty</option>
           <option value="electronics">Electronics</option>
           <option value="others">Others</option>
         </select>
         {errors["product-category"] && <p className="error-message">{errors["product-category"].message}</p>}
-        
+
         <label htmlFor="price">Price</label>
         <input type="number" {...register("price", { min: { value: 1, message: "Minimum price is 1" }, required: "Price is required" })} className="input-field" />
         {errors.price && <p className="error-message">{errors.price.message}</p>}
-        
+
         <label htmlFor="discount">Discount in %</label>
-        <input type="number" {...register("discount")} className="input-field" />
-        
+        <input type="number" {...register("discount", { min: { value: 0, message: "Minimum discount is 0" } })} className="input-field" />
+        {errors.discount && <p className="error-message">{errors.discount.message}</p>}
+
         <div className="field-array-container">
           <label>Images</label>
           {imageFields.map((field, index) => (
@@ -60,7 +84,7 @@ const ProductList = () => {
           ))}
           <button type="button" onClick={() => appendImage({ url: '' })} className="add-button">Add Image</button>
         </div>
-        
+
         <div className="field-array-container">
           <label>Specifications</label>
           {specFields.map((field, index) => (
@@ -72,10 +96,11 @@ const ProductList = () => {
           ))}
           <button type="button" onClick={() => appendSpec({ spec: '' })} className="add-button">Add Specification</button>
         </div>
-        
+
         <label htmlFor="stock">Available Stock</label>
-        <input type="number" {...register("stock")} className="input-field" />
-        
+        <input type="number" {...register("stock", { min: { value: 10, message: "Minimum stock is 10" }, required: "stock is required" })} className="input-field" />
+        {errors.stock && <p className="error-message">{errors.stock.message}</p>}
+
         <input type="submit" className="submit-button" />
       </form>
     </div>
